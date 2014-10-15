a0ee8cde4fd8f7281b70103612bb0b80e91d77b1
bertrand <guerrerobertrand@gmail.com>
2014-10-15 10:22:13 +0200

    Projet= Delannoy : Chapitre 11 = Threads: suite non terminé

--- Delannoy_projet1/src/Chaptire11_Threads.java
+++ Delannoy_projet1/src/Chaptire11_Threads.java
@@ -3,7 +3,8 @@ public class Chaptire11_Threads {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	 { 
+	}
+}
 /*//METHODE SUPER CLASSE THREAD, objets thread, run,...
 		 Ecrit e1 = new Ecrit ("bonjour ", 10, 5) ;
 		 Ecrit e2 = new Ecrit ("bonsoir ", 15, 10) ;
@@ -80,7 +81,7 @@ class Ecrit implements Runnable
 		 private long attente ;
 }	*/	 
 		 
-		 Ecrit e1 = new Ecrit ("bonjour ", 5) ;
+/*		 Ecrit e1 = new Ecrit ("bonjour ", 5) ;
 		 Ecrit e2 = new Ecrit ("bonsoir ", 10) ;
 		 Ecrit e3 = new Ecrit ("\n", 35) ;
 		 e1.start() ;
@@ -120,4 +121,63 @@ class Ecrit extends Thread
 		 }
 		 private String texte ;
 		 private long attente ;
+}*/
+
+/*		//SYNCRONISATION DE THREAD EX: nombre et calcul de son carrŽ + affichage de valeur n et du rŽsultat (carrŽ) => (2 threads)
+		 Nombres nomb = new Nombres() ;
+		 Thread calc = new ThrCalc (nomb) ;
+		 Thread aff = new ThrAff (nomb) ;
+		 System.out.println ("Suite de carres - tapez retour pour arreter") ;
+		 calc.start() ; aff.start() ;
+		 Clavier.lireString() ;
+		 calc.interrupt() ; aff.interrupt() ;
+	 }
+	}	
+	class Nombres
+	{ public synchronized void calcul()
+	{ n++ ;
+	carre = n*n ;
+	}
+	public synchronized void affiche ()
+	{ System.out.println (n + " a pour carre " + carre) ;
+	}
+	private int n=0, carre ;
+	}
+
+class ThrCalc extends Thread{
+	public ThrCalc (Nombres nomb){
+		this.nomb=nomb;
+	}
+	public void run(){
+		try{
+			while(!interrupted()){
+				nomb.calcul();
+				sleep(50);
+			}
+		}
+		catch(InterruptedException e){}
+	}
+	private Nombres nomb;
 }
+
+class ThrAff extends Thread{
+	public ThrAff (Nombres nomb){
+		this.nomb=nomb;
+	}
+	public void run(){
+		try{
+			while(!interrupted()){
+				nomb.affiche();
+				sleep(75);
+			}
+		}
+		catch(InterruptedException e){}
+	}	
+	private Nombres nomb;
+
+}
+*/
+
+		 
+		 
+		 
